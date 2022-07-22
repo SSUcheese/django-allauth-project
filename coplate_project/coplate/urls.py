9c961a07fb43d6ed4e2c05b1fa505ca3ad7cf09d
cheese <zoojj3319@gmail.com>
2022-07-22 23:37:53 +0900

    Update detail view page to look better

--- coplate_project/coplate/urls.py
+++ coplate_project/coplate/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path(
+        "reviews/<int:review_id>/",
+        views.ReviewDetailListView.as_view(),
+        name="review-detail",
+        )
 ]
--- coplate_project/coplate/views.py
+++ coplate_project/coplate/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from allauth.account.views import PasswordChangeView
 from .models import User, Review
 
@@ -12,6 +12,12 @@ class IndexView(ListView):
     context_object_name = "reviews" # 데이터 긁어서 템플릿에 넘길 때 그 이름은 reivews로 한다.
     paginate_by = 4 
     ordering = ["-dt_created"]
+
+class ReviewDetailListView(DetailView):
+    model = Review
+    template_name = "coplate/review_detail.html"
+    pk_url_kwarg = "review_id"
+# 참고로 detailView와 같이 object를 하나만 갖고 오는 view에선 context_object_name의 default 값이 model의 이름이 된다.
 
 class CustomPasswordChangeView(PasswordChangeView): # 이거 기존 부모코드에서 있는 success_url 자식코드에서 오버라이딩 해서 사용하는 구조임
     def get_success_url(self): # 어떤 form이 성공적으로 처리되면 어디로 redirection 핳건지 정해주는 함수
